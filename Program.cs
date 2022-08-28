d8f0b5d7923a1be337331c1d8e8d661d1d49fa51
AnnaShilova <Anna.Shilova@spcpu.ru>
2022-08-28 15:27:08 +0300

    FirstC

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void SecondNum (){
+    int num = new Random().Next(100,1000);
+    Console.WriteLine("Наше число - " + num);
+    int snum = num/10;
+    int result = snum %10;
+    Console.WriteLine("Ответик: " + result);
+}
+
+SecondNum();
+
